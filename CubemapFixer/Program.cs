4bd3645af7f3e21bf14f63666a4110d3e686977b
FallMiner-SKITLSE <52322048+PastMC@users.noreply.github.com>
2023-04-08 20:50:34 +0300

    Some clean code

--- CubemapFixer/Program.cs
+++ CubemapFixer/Program.cs
@@ -8,8 +8,8 @@ namespace CubemapFixer
     {
         static void Main(string[] args)
         {
+            // args[0] = pathToBreaked Cubemap args[1] = path to save fixed cubemap
             Console.WriteLine(args.Length);
-            // Specify the source and destination file paths
             if (args != null && args.Length == 2)
             {
                 string sourceFilePath = args[0];
@@ -21,30 +21,31 @@ namespace CubemapFixer
 
                 // Create a new bitmap for the destination image
                 Bitmap destinationImage = new Bitmap(sourceImage.Width * 4, sourceImage.Width * 3);
+                // Rectangle for cut and paste the images
                 Rectangle bottomSection1 = new Rectangle(0, 0, sourceImage.Width, sourceImage.Width);
-                Bitmap bottomImage1 = sourceImage.Clone(bottomSection1, sourceImage.PixelFormat);
-                // Save the new image to a file
-                bottomImage1.RotateFlip(RotateFlipType.Rotate180FlipX);
                 Rectangle bottomSection2 = new Rectangle(0, sourceImage.Width, sourceImage.Width, sourceImage.Width);
-                Bitmap bottomImage2 = sourceImage.Clone(bottomSection2, sourceImage.PixelFormat);
-                // Save the new image to a file
-                bottomImage2.RotateFlip(RotateFlipType.Rotate180FlipX);
                 Rectangle bottomSection = new Rectangle(0, sourceImage.Width * 2, sourceImage.Width, sourceImage.Width);
-                Bitmap bottomImage = sourceImage.Clone(bottomSection, sourceImage.PixelFormat);
-                // Save the new image to a file
-                bottomImage.RotateFlip(RotateFlipType.Rotate180FlipY);
                 Rectangle bottomSection4 = new Rectangle(0, sourceImage.Width * 3, sourceImage.Width, sourceImage.Width);
-                Bitmap bottomImage4 = sourceImage.Clone(bottomSection4, sourceImage.PixelFormat);
-                // Save the new image to a file
-                bottomImage4.RotateFlip(RotateFlipType.Rotate180FlipY);
                 Rectangle bottomSection5 = new Rectangle(0, sourceImage.Width * 4, sourceImage.Width, sourceImage.Width);
-                Bitmap bottomImage5 = sourceImage.Clone(bottomSection5, sourceImage.PixelFormat);
-                // Save the new image to a file
-                bottomImage5.RotateFlip(RotateFlipType.Rotate180FlipX);
                 Rectangle bottomSection6 = new Rectangle(0, sourceImage.Width * 5, sourceImage.Width, sourceImage.Width);
+                
+                //Create Bitmaps
+                Bitmap bottomImage1 = sourceImage.Clone(bottomSection1, sourceImage.PixelFormat);
+                Bitmap bottomImage2 = sourceImage.Clone(bottomSection2, sourceImage.PixelFormat);
+                Bitmap bottomImage = sourceImage.Clone(bottomSection, sourceImage.PixelFormat);
+                Bitmap bottomImage4 = sourceImage.Clone(bottomSection4, sourceImage.PixelFormat);
+                Bitmap bottomImage5 = sourceImage.Clone(bottomSection5, sourceImage.PixelFormat);
                 Bitmap bottomImage6 = sourceImage.Clone(bottomSection6, sourceImage.PixelFormat);
-                // Save the new image to a file
+                
+                // Flip Images
+                bottomImage1.RotateFlip(RotateFlipType.Rotate180FlipX);
+                bottomImage2.RotateFlip(RotateFlipType.Rotate180FlipX);
+                bottomImage.RotateFlip(RotateFlipType.Rotate180FlipY);
+                bottomImage4.RotateFlip(RotateFlipType.Rotate180FlipY);
+                bottomImage5.RotateFlip(RotateFlipType.Rotate180FlipX);
                 bottomImage6.RotateFlip(RotateFlipType.Rotate180FlipX);
+                
+                //Create Graphics
                 Graphics graphics = Graphics.FromImage(destinationImage);
 
                 // Draw the foreground image on the composite image at (x, y)
@@ -54,6 +55,8 @@ namespace CubemapFixer
                 graphics.DrawImage(bottomImage4, sourceImage.Width, sourceImage.Width * 2);
                 graphics.DrawImage(bottomImage5, sourceImage.Width * 3, sourceImage.Width);
                 graphics.DrawImage(bottomImage6, sourceImage.Width, sourceImage.Width);
+                
+                //Save Image
                 destinationImage.Save(destinationFilePath, System.Drawing.Imaging.ImageFormat.Png);
 
                 // Clean up
@@ -66,24 +69,6 @@ namespace CubemapFixer
                 bottomImage5.Dispose();
                 bottomImage6.Dispose();
                 destinationImage.Dispose();
-                // Create a new bitmap for the destination image
-                /*Bitmap destinationImage = new Bitmap(sourceImage.Width * 3, sourceImage.Width * 4);
-
-                // Copy the pixels from the source image to the destination image
-                /*for (int x = 0; x < sourceImage.Width; x++)
-                {
-                    for (int y = 0; y < sourceImage.Height; y++)
-                    {
-                        destinationImage.SetPixel(x, y, sourceImage.GetPixel(x, y));
-                    }
-                }
-
-                // Save the destination image to a file
-                destinationImage.Save(destinationFilePath, System.Drawing.Imaging.ImageFormat.Png);
-
-                // Clean up
-                sourceImage.Dispose();
-                destinationImage.Dispose();*/
             } else
             {
                 Console.WriteLine("ERROR to start program need 2 paths");
